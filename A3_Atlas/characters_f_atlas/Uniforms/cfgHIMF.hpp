bcad87070d1ac804b33c88c87c4ef70ae57ffc4a
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-05-24 10:34:02 -0700

    Commandos Completion and Marar
    Completed HIMF Naval Commandos and added Marari CBRN and vehicle coveralls.

--- A3_Atlas/characters_f_atlas/Uniforms/cfgHIMF.hpp
+++ A3_Atlas/characters_f_atlas/Uniforms/cfgHIMF.hpp
@@ -70,7 +70,7 @@ class Atlas_U_B_H_Soldier_commando_F: Uniform_Base
 {
 	author = $STR_A3_A_AveryTheKitty;
 	scope = public;
-	displayName = $STR_A3_A_CfgWeapons_U_B_H_Soldier_F0;
+	displayName = $STR_A3_A_CfgWeapons_U_B_CombatUniform_commando0;
 	picture = "\A3_Atlas\Characters_F_Atlas\Uniforms\Data\UI\icon_U_B_H_Soldier_F_CA.paa";
 	model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
 	hiddenSelections[] = {camo};
@@ -87,7 +87,7 @@ class Atlas_U_B_H_Soldier_commando_shortsleeve_F: Uniform_Base
 {
 	author = $STR_A3_A_AveryTheKitty;
 	scope = public;
-	displayName = $STR_A3_A_CfgWeapons_U_B_H_Soldier_F0;
+	displayName = $STR_A3_A_CfgWeapons_U_B_CombatUniform_commando_vest0;
 	picture = "\A3_Atlas\Characters_F_Atlas\Uniforms\Data\UI\icon_U_B_H_Soldier_F_CA.paa";
 	model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
 	hiddenSelections[] = {camo};
