f0b1a7b19a6c0751ecb6e07efbc8f22371688570
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-05-26 13:10:22 -0700

    Marar Groups
    Added Marar infantry groups.

--- A3_Atlas/characters_f_atlas/cfgGroups.hpp
+++ A3_Atlas/characters_f_atlas/cfgGroups.hpp
@@ -496,6 +496,49 @@ class CfgGroups
 				};
 			};
 		};
+		class Atlas_BLU_M_F
+		{
+			name = $STR_A3_A_CfgFactionClasses_BLU_M_F0;
+			class Infantry
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Infantry0;
+				class B_M_InfSquad
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfSquad0;
+					side = TWest;
+					faction = Atlas_BLU_H_F;
+					icon = "\A3\UI_F\Data\Map\Markers\NATO\b_inf.paa";
+					GROUP_UNIT(0,TWest,Atlas_B_M_SL_F,SERGEANT,0,0)
+					GROUP_UNIT(1,TWest,Atlas_B_M_RadioOperator_F,PRIVATE,5,-5)
+					GROUP_UNIT(2,TWest,Atlas_B_M_Soldier_LAT_F,CORPORAL,-5,-5)
+					GROUP_UNIT(3,TWest,Atlas_B_M_M_F,PRIVATE,10,-10)
+					GROUP_UNIT(4,TWest,Atlas_B_M_TL_F,SERGEANT,-10,-10)
+					GROUP_UNIT(5,TWest,Atlas_B_M_AR_F,CORPORAL,15,-15)
+					GROUP_UNIT(6,TWest,Atlas_B_M_A_F,PRIVATE,-15,-15)
+					GROUP_UNIT(7,TWest,Atlas_B_M_medic_F,PRIVATE,20,-20)
+				};
+				class B_M_InfTeam
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfTeam0;
+					side = TWest;
+					faction = Atlas_BLU_H_F;
+					icon = "\A3\UI_F\Data\Map\Markers\NATO\b_inf.paa";
+					GROUP_UNIT(0,TWest,Atlas_B_M_TL_F,SERGEANT,0,0)
+					GROUP_UNIT(1,TWest,Atlas_B_M_AR_F,CORPORAL,5,-5)
+					GROUP_UNIT(2,TWest,Atlas_B_M_GL_F,PRIVATE,-5,-5)
+					GROUP_UNIT(3,TWest,Atlas_B_M_Soldier_LAT_F,PRIVATE,10,-10)
+				};
+				class B_M_InfSentry
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfSentry0;
+					side = TWest;
+					faction = Atlas_BLU_H_F;
+					icon = "\A3\UI_F\Data\Map\Markers\NATO\b_inf.paa";
+					GROUP_UNIT(0,TWest,Atlas_B_M_GL_F,CORPORAL,0,0)
+					GROUP_UNIT(1,TWest,Atlas_B_M_Soldier_F,PRIVATE,5,-5)
+				};
+			};
+		};
 	};
 	class East
 	{
