93d7d23c51cab0e5c0dd42aeddf1338c609c834d
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-06-21 21:00:48 -0700

    FUCK Okay ADF is fixed
    Fuck load of config bullshit squared away

--- A3_Atlas/supplies_f_atlas/Bags/cfgVehicles.hpp
+++ A3_Atlas/supplies_f_atlas/Bags/cfgVehicles.hpp
@@ -146,6 +146,14 @@ class CfgVehicles
 		//picture = "\A3_Atlas\Supplies_F_Atlas\Bags\Data\UI\icon_B_Kitbag_flecktarn_CA.paa";
 		hiddenSelectionsTextures[] = {"\A3_Atlas\Supplies_F_Atlas\Bags\Data\backpack_fast_flecktarn_CO.paa"};
 	};
+	class B_Kitbag_aucamo_F: B_Kitbag_Base
+	{
+		author = $STR_A3_A_AveryTheKitty;
+		scope = public;
+		displayName = $STR_A3_A_CfgVehicles_B_Kitbag_aucamo0;
+		//picture = "\A3_Atlas\Supplies_F_Atlas\Bags\Data\UI\icon_B_Kitbag_flecktarn_CA.paa";
+		hiddenSelectionsTextures[] = {"\A3_Atlas\Supplies_F_Atlas\Bags\Data\backpack_fast_aucamo_CO.paa"};
+	};
 	class B_Carryall_flecktarn: B_Carryall_Base
 	{
 		author = $STR_A3_A_AveryTheKitty;
@@ -153,6 +161,14 @@ class CfgVehicles
 		displayName = $STR_A3_A_CfgVehicles_B_Carryall_flecktarn0;
 		//picture = "\A3_Atlas\Supplies_F_Atlas\Bags\Data\UI\icon_B_Carryall_flecktarn_CA.paa";
 		hiddenSelectionsTextures[] = {"\A3_Atlas\Supplies_F_Atlas\Bags\Data\backpack_tortila_flecktarn_CO.paa"};
+	};
+	class B_Carryall_owcamo: B_Carryall_Base
+	{
+		author = $STR_A3_A_AveryTheKitty;
+		scope = public;
+		displayName = $STR_A3_A_CfgVehicles_B_Carryall_owcamo0;
+		picture = "\A3_Atlas\Supplies_F_Atlas\Bags\Data\UI\icon_B_Carryall_owcamo_CA.paa";
+		hiddenSelectionsTextures[] = {"\A3_Atlas\Supplies_F_Atlas\Bags\Data\backpack_tortila_whex_CO.paa"};
 	};
 	class B_Carryall_aucamo_F: B_Carryall_Base
 	{
