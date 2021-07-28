baea540f4b0a794c71a0ae3d1cbb354e87b93fc9
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-07-27 21:07:43 -0700

    AH64 Apache
    AH-64 Apache in ADF vehicle camo has been implemented.

--- A3_Atlas/air_f_atlas/Heli_Attack_03/cfgVehicles.hpp
+++ A3_Atlas/air_f_atlas/Heli_Attack_03/cfgVehicles.hpp
@@ -20,8 +20,21 @@ class CfgVehicles
 				};
 				factions[] = {};
 			};
+			class ADF
+			{
+				displayName = $STR_A3_A_TEXTURESOURCES_ADF0;
+				author = "BranFlakes";
+				textures[] =
+				{
+                    "\A3_Atlas\Air_F_Atlas\Heli_Attack_03\Data\Heli_Attack_03_Body_ADF_CO.paa",
+                    "\A3_Atlas\Air_F_Atlas\Heli_Attack_03\Data\Heli_Attack_03_details_ADF_CO.paa",
+                    "\A3_Atlas\Air_F_Atlas\Heli_Attack_03\Data\Heli_Attack_03_adds_ADF_CO.paa"
+				};
+				factions[] = {};
+			};
         };
     };
     /* Factions */
     #include "cfgMarar.hpp"   // Marar
+    #include "cfgADF.hpp"     //ADF
 };
