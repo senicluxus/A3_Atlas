8eb7b0f0c7fac7c549ae065fd7c661ef3c987a6b
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-07-27 22:32:50 -0700

    CDF Uniform
    Added full and rolled CDF uniforms for CDF UN peacekeepers.

--- A3_Atlas/characters_f_atlas/Uniforms/cfgUNO.hpp
+++ A3_Atlas/characters_f_atlas/Uniforms/cfgUNO.hpp
@@ -32,3 +32,46 @@ class Atlas_U_I_U_CombatUniform_shortsleeve_UNO: Uniform_Base
 		mass = 40;
 	};
 };
+class Atlas_U_I_UW_CombatUniform_UNO: Uniform_Base
+{
+	author = "BranFlakes";
+	scope = public;
+	displayName = "Combat Fatigues [Chernarus]";
+	//picture = "\A3_Atlas\Characters_F_Atlas\Uniforms\Data\UI\icon_U_I_I_CombatUniform_olive_CA.paa";
+	model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+	hiddenSelections[] = {camo};
+	hiddenSelectionsTextures[] =
+	{
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues.paa",
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues_Pants.paa",
+		"\A3\Characters_F_Enoch\Uniforms\Data\I_E_Soldier_01_gloves_black_CO.paa"
+	};
+	class ItemInfo: UniformItem
+	{
+		uniformModel = "-";
+		uniformClass = Atlas_I_UW_CombatFatigues_01_UNO_F;
+		containerClass = Supply40;
+		mass = 40;
+	};
+};
+class Atlas_U_I_UW_CombatUniform_shortsleeve_UNO: Uniform_Base
+{
+	author = "BranFlakes";
+	scope = public;
+	displayName = "Combat Fatigues (Rolled-up) [Chernarus]";
+	//picture = "\A3_Atlas\Characters_F_Atlas\Uniforms\Data\UI\icon_U_I_I_CombatUniform_shortsleeve_olive_CA.paa";
+	model = "\A3\Characters_F\Common\Suitpacks\suitpack_universal_F.p3d";
+	hiddenSelections[] = {camo};
+	hiddenSelectionsTextures[] =
+	{
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues.paa",
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues_Pants.paa",
+	};
+	class ItemInfo: UniformItem
+	{
+		uniformModel = "-";
+		uniformClass = Atlas_I_UW_CombatFatigues_01_RolledUp_UNO_F;
+		containerClass = Supply40;
+		mass = 40;
+	};
+};
--- A3_Atlas/characters_f_atlas/cfgUniforms.hpp
+++ A3_Atlas/characters_f_atlas/cfgUniforms.hpp
@@ -464,6 +464,29 @@ class Atlas_I_U_CombatFatigues_01_RolledUp_UNO_F: I_soldier_lite_F
 	uniformClass = Atlas_U_I_U_CombatUniform_shortsleeve_UNO;
 	hiddenSelectionsTextures[] = {"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_Sahrani_CombatFatigues_Pants.paa"};
 };
+class Atlas_I_UW_CombatFatigues_01_UNO_F: I_E_Uniform_01_F
+{
+  	scope = protected;
+  	scopeCurator = private;
+	uniformClass = Atlas_U_I_UW_CombatUniform_UNO;
+	hiddenSelectionsTextures[] =
+	{
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues.paa",
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues_Pants.paa",
+		"\A3\Characters_F_Enoch\Uniforms\Data\I_E_Soldier_01_gloves_black_CO.paa"
+	};
+};
+class Atlas_I_UW_CombatFatigues_01_RolledUp_UNO_F: I_E_Uniform_01_shortsleeve_F
+{
+  	scope = protected;
+  	scopeCurator = private;
+	uniformClass = Atlas_U_I_UW_CombatUniform_shortsleeve_UNO;
+	hiddenSelectionsTextures[] =
+	{
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues.paa",
+		"\A3_Atlas\Characters_F_Atlas\Uniforms\Data\Uniform_CDF_CombatFatigues_Pants.paa",
+	};
+};
 /* Venezuela */
 class Atlas_O_VZ_Officer_oli_CO_F: O_A_Soldier_Base_F
 {
