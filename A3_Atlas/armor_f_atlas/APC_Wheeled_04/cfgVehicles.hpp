661e9bbeeeb1bd562b3d268a95387adb64d41dc3
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-09-15 16:39:11 +0100

    UNO BTR TextureSources

--- A3_Atlas/armor_f_atlas/APC_Wheeled_04/cfgVehicles.hpp
+++ A3_Atlas/armor_f_atlas/APC_Wheeled_04/cfgVehicles.hpp
@@ -2,6 +2,25 @@ class CfgVehicles
 {
     /* Inheritance Tree */
     class O_R_APC_Wheeled_04_cannon_F;
+    {
+        class TextureSources
+        {
+            class UNO
+            {
+                displayName = $STR_A3_A_TEXTURESOURCES_UNO0;
+                author = "BranFlakes"
+                materials[] =
+                {
+                    "\A3_Atlas\Armor_F_Atlas\APC_Wheeled_04\Data\APC_Wheeled_04_body_UNO_CO.paa",
+                    "\A3_Atlas\Armor_F_Atlas\APC_Wheeled_04\Data\APC_Wheeled_04_body2_UNO_CO.paa",
+                    "\A3_Atlas\Armor_F_Atlas\APC_Wheeled_04\Data\APC_Wheeled_04_tow_UNO_CO.paa",
+                    "\A3_Aegis\Armor_F_Aegis\Data\camonet_RUS_Green_CO.paa",
+                    "\A3_Aegis\Armor_F_Aegis\Data\cage_RUkhk_CO.paa"
+                };
+            };
+
+        };
+    };
 
     /* Factions */
     #include "cfgVenezuela.hpp"   // Venezuela
