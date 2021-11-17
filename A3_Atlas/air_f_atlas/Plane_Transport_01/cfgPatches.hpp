c0b906b731fa5842a760534e619382b4c011a26f
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-11-16 22:10:38 -0800

    ADF C-192 Samson

--- A3_Atlas/air_f_atlas/Plane_Transport_01/cfgPatches.hpp
+++ A3_Atlas/air_f_atlas/Plane_Transport_01/cfgPatches.hpp
@@ -7,9 +7,6 @@ class CfgPatches
 		requiredVersion = 0.1;
 		units[] =
         {
-            // Germany
-            Atlas_B_G_Plane_Transport_01_infantry_F,
-            Atlas_B_G_Plane_Transport_01_vehicle_F,
 
             // ADF
             Atlas_B_A_Plane_Transport_01_infantry_F,
