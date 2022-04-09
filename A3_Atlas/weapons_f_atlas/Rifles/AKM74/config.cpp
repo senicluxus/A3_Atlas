796bc8d77fad454cb0d8ac52c147e6aefdd8430c
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2022-04-08 22:56:32 -0700

    AK-74M Texture Fixes

--- A3_Atlas/weapons_f_atlas/Rifles/AKM74/config.cpp
+++ A3_Atlas/weapons_f_atlas/Rifles/AKM74/config.cpp
@@ -62,15 +62,17 @@ class CfgWeapons
 		author = "Grave";
 		baseWeapon = arifle_AKM74_polymer;
 		scope = public;
+		hiddenSelections[] = {"camo_1","camo_2","camo_3"};
 		displayName = $STR_A3_A_CfgWeapons_arifle_AKM74_polymer_F0;
-		hiddenSelectionsTextures[] = {"A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_01_black_CO.paa","A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_01_wood_CO.paa","\A3\Weapons_F_Exp\Rifles\AKM\Data\akm_steel_mag_co.paa"};
+		hiddenSelectionsTextures[] = {"A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_01_black_CO.paa","A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_02_black_CO.paa","\A3\Weapons_F_Exp\Rifles\AKM\Data\akm_steel_mag_co.paa"};
 	};
 	class arifle_AKM74_polymer_alt: arifle_AKM74_F
 	{
 		author = "Grave";
 		baseWeapon = arifle_AKM74_polymer_alt;
 		scope = public;
+		hiddenSelections[] = {"camo_1","camo_2","camo_3"};
 		displayName = $STR_A3_A_CfgWeapons_arifle_AKM74_polymer_alt_F0;
-		hiddenSelectionsTextures[] = {"A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_02_black_CO.paa","A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_02_wood_CO.paa","\A3\Weapons_F_Exp\Rifles\AKM\Data\akm_steel_mag_co.paa"};
+		hiddenSelectionsTextures[] = {"A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_01_wood_CO.paa","A3_Atlas\weapons_f_atlas\Rifles\AKM74\Data\arifle_AK74_02_wood_CO.paa","\A3\Weapons_F_Exp\Rifles\AKM\Data\akm_steel_mag_co.paa"};
 	};
 };
